155c557b2513cafe1df99a0019ba739e9053c1ce
Leon Thorne <ldthorne@brandeis.edu>
2015-11-10 16:58:00 -0500

    able to load the index page. no errors at the momemt

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ var app = express();
 var port = 3000;
 var bodyParser = require("body-parser");
 var swig = require("swig");
+var routes = require('./routes/');
+var fs = require("fs")
+var mime = require("mime")
 
-
-
-app.use(function(req, res, next){ //guy at gate
-	console.log("Check yourself before you req yourself");
-	next();
+app.use(function(req, res, next) { //guy at gate
+    console.log("Check yourself before you req yourself");
+    next();
 });
+
+app.use(function(req, res, next) {
+  console.log(req.path) // stylesheets/style.css
+  var mimeType = mime.lookup(req.path)
+  fs.readFile('./public/' + req.path, function(err, fileBuffer) {
+    if(err) return next()
+    res.header('Content-Type', mimeType)
+    res.send(fileBuffer)
+  })
+})
+    
+
+
+app.use('/', routes);
+
+app.get("/")
 
 app.engine("html", require("swig").renderFile);
 
@@ -17,15 +34,10 @@ app.set("view engine", "html");
 
 app.set("views", __dirname + "/views/");
 
-swig.setDefaults({cache : false});
-
-var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-app.get("/", function(req, res){
-	console.log("hi");
-	res.render( 'index', {title: 'Hall of Fame', people: people});
+swig.setDefaults({
+    cache: false
 });
 
-app.listen(port, function(){
-	console.log("Listening on port " + port);
+app.listen(port, function() {
+    console.log("Listening on port " + port);
 });
-
